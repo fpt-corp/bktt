e8bce56094f99723d6acc5371ae32d963977721b
Minh Pham <pcongminh1@gmail.com>
2020-10-06 16:14:48 +0700

    add extension CodeMirror

---
+++ extensions/CodeMirror/Gruntfile.js
@@ -0,0 +1,35 @@
+/* eslint-env node, es6 */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				reportUnusedDisableDirectives: true,
+				extensions: [ '.js', '.json' ],
+				cache: true
+			},
+			all: [
+				'**/*.{js,json}',
+				'!resources/lib/**',
+				'!{vendor,node_modules}/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.{css,less}',
+				'!resources/lib/**',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		banana: {
+			all: 'i18n/'
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
