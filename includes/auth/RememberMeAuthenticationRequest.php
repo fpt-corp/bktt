5aaffa3efaf27091f28894f9298dcb49d51077da
TranTheTrung <trungtt3@fpt.com.vn>
2020-11-12 12:37:35 +0700

    trying to have remember-me by default

--- includes/auth/RememberMeAuthenticationRequest.php
+++ includes/auth/RememberMeAuthenticationRequest.php
@@ -68,6 +68,7 @@ class RememberMeAuthenticationRequest extends AuthenticationRequest {
 				'help' => wfMessage( 'authmanager-userlogin-remembermypassword-help' ),
 				'optional' => true,
 				'skippable' => true,
+				'default' => true
 			]
 		];
 	}
