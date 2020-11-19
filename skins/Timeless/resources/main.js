f37dde5d763592e3ef61de1459779201fe52a938
TranTheTrung <trungtt3@fpt.com.vn>
2020-11-19 09:04:28 +0700

    Update main.js

--- skins/Timeless/resources/main.js
+++ skins/Timeless/resources/main.js
@@ -1,8 +1,10 @@
 $( function () {
 	$( '#sidebar-button' ).on( 'click', function () {
 		if ($('#mw-site-navigation').css('display') == 'none') {
+			console.log('ddd')
 			$('#mw-site-navigation').css('display','block')
 		} else {
+			console.log('bbbb')
 		        $('#mw-site-navigation').css('display','none')
 		}
 	} );
