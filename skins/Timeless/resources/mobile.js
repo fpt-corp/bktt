aaec15c71ce93070e0dfb2476b2a5e41997b46a2
TranTheTrung <trungtt3@fpt.com.vn>
2020-11-19 11:30:55 +0700

    Update mobile.js

--- skins/Timeless/resources/mobile.js
+++ skins/Timeless/resources/mobile.js
@@ -53,10 +53,10 @@ $( function () {
 	$( '#sidebar-button' ).on( 'click', function () {
 		if ($('#mw-site-navigation').css('display') == 'none') {
 			$('#mw-site-navigation').css('display','block')
-			$('#sidebar-button').css('background-image','url("'+'@{resourcesRoot}'+'/skins/Timeless/resources/images/nume.svg")')
+			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/nume.svg)')
 		} else {
 		        $('#mw-site-navigation').css('display','none')
-			$('#sidebar-button').css('background-image','url("'+'@{resourcesRoot}'+'/skins/Timeless/resources/images/menu.svg")')
+			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)')
 		}
 	} );
 	
