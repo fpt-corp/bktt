fe95e99c2f7aa56c189d2d990217f3bf62d4b475
TranTheTrung <trungtt3@fpt.com.vn>
2020-12-09 14:15:51 +0700

    Update TimelessTemplate.php

--- skins/Timeless/includes/TimelessTemplate.php
+++ skins/Timeless/includes/TimelessTemplate.php
@@ -523,7 +523,7 @@ class TimelessTemplate extends BaseTemplate {
 
 		$html .= Html::rawElement( 'form', [ 'action' => $this->get( 'wgScript' ), 'id' => 'searchform' ],
 			Html::rawElement( 'div', [ 'id' => 'simpleSearch' ],
-				$this->makeSearchInput( ['id' => 'searchInput', 'placeholder' => 'Tìm trong BÁCH KHOA TOÀN THƯ VIỆT NAM ...'] ) .
+				$this->makeSearchInput( ['id' => 'searchInput', 'placeholder' => 'Tìm ở Bách khoa Toàn thư Việt Nam ...'] ) .
 				Html::hidden( 'title', $this->get( 'searchtitle' ) ) .
 				//$this->makeSearchButton(
 				//	'fulltext',
