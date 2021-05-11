4d1944cc438fde3ccbb6621f992d3c936382597c
TranTheTrung <trungtt3@fpt.com.vn>
2021-05-11 12:36:21 +0700

    allow TemplateStyles to have more effect

--- skins/Timeless/includes/TimelessTemplate.php
+++ skins/Timeless/includes/TimelessTemplate.php
@@ -48,7 +48,7 @@ class TimelessTemplate extends BaseTemplate {
 
 		$html .= Html::rawElement( 'div', [ 'id' => 'mw-content-container', 'class' => 'ts-container' ],
 			Html::rawElement( 'div', [ 'id' => 'mw-content-block'],
-				Html::rawElement( 'div', [ 'id' => 'mw-content-wrapper' ],
+				Html::rawElement( 'div', [ 'id' => 'mw-content-wrapper' , 'class' => 'mw-parser-output' ],
 					$this->getContentBlock() .
 					$this->getAfterContent()
 				) .
