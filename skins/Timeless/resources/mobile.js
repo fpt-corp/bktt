2c9ae3769458be34aa058e42c52d26c2115adba7
TranTheTrung <trungtt3@fpt.com.vn>
2021-05-15 08:43:04 +0700

    Update mobile.js

--- skins/Timeless/resources/mobile.js
+++ skins/Timeless/resources/mobile.js
@@ -2,22 +2,21 @@
 
 $( function () {
 	
-	$( '#hamburger-menu-icon' ).on( 'click', function () {
-		$('#hamburger-menu').css('display', 'flex')
-	} );
+	$('body').on('click', function () {
+		if ($('#mw-site-navigation').css('display') == 'block') {
+			$('#mw-site-navigation').css('display','none');
+			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)');
+		}
+	});
 
-	$( '#hamburger-menu' ).on( 'click', function () {
-		$('#hamburger-menu').css('display', 'none')
-	} );
-	
 	// Nút trên bên trái 
 	$( '#sidebar-button' ).on( 'click', function () {
 		if ($('#mw-site-navigation').css('display') == 'none') {
-			$('#mw-site-navigation').css('display','block')
-			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/nume.svg)')
+			$('#mw-site-navigation').css('display','block');
+			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/nume.svg)');
 		} else {
-		        $('#mw-site-navigation').css('display','none')
-			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)')
+		        $('#mw-site-navigation').css('display','none');
+			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)');
 		}
 	} );
 	
