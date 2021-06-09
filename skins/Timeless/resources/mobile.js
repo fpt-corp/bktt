c48092911fc15ef2971a97e79f13ef135abbc1ce
TranTheTrung <trungtt3@fpt.com.vn>
2021-06-09 16:42:19 +0700

    Update mobile.js

--- skins/Timeless/resources/mobile.js
+++ skins/Timeless/resources/mobile.js
@@ -5,10 +5,12 @@ $( function () {
 	$( '#sidebar-button' ).on( 'click', function () {
 		if ($('#mw-site-navigation').css('display') == 'none') {
 			$('#mw-site-navigation').css('display','block');
-			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/nume.svg)');
+			$('#sidebar-button').css('mask-image','url(https://bktt.vn/skins/Timeless/resources/images/nume.svg)');
+			$('#sidebar-button').css('-webkit-mask-image','url(https://bktt.vn/skins/Timeless/resources/images/nume.svg)');
 		} else {
 		        $('#mw-site-navigation').css('display','none');
-			$('#sidebar-button').css('background-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)');
+			$('#sidebar-button').css('mask-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)');
+			$('#sidebar-button').css('-webkit-mask-image','url(https://bktt.vn/skins/Timeless/resources/images/menu.svg)');
 		}
 	} );
 	
